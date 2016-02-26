e74a5e317d63c659742755e09d5075d9253b1467
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2016-02-26 16:15:22 +0100

    stores pretty in db

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-Template.callout.events({
+Template.dashboard.events({
     "click #btnReadCSV": function storeData(event, template) {
         var result = {};
 
@@ -8,35 +8,35 @@ Template.callout.events({
 
 
             console.log("data[1].length = ", data[1].length, "\n", "data.length = ", data.length, "\n", "freq = ", freq);
-            for (var i = 0; i < data[data[1].length-1].length; i++) {
-                var j = 0;
+            for (var nRows = 0; nRows < data[data[1].length-1].length; nRows++) {
+                var nColumns = 0;
                 var k = 0;
-                var n = -1;
+                var nObjValue = -1;
                 do {
-                    if (j == 0) {
+                    if (nColumns == 0) {
                         result = {
-                            type: data[j][i],
+                            type: data[nColumns][nRows],
                             values: {},
                         }
-                        j++;
+                        nColumns++;
                     } else {
-                        n++;
-                        for (var k = 0; k < freq; k++) {
+                        nObjValue++;
+                        for (var k = 0; k < freq && nColumns + k < data.length - 1; k++) {
                             if (k == 0) {
-                                result.values[n] = {};
+                                result.values[nObjValue] = {};
 
                             };
-                            // console.log("data[", j, " + ", k, "][", i, "] = ", data[j + k][i]);
-                            value = Number(data[j + k][i].replace(',', '.'));
-                            result.values[n][k] = value;
+                            // console.log("data[", nColumns, " + ", k, "][", nRows, "] = ", data[nColumns + k][nRows]);
+                            value = Number(data[nColumns + k][nRows].replace(',', '.'));
+                            result.values[nObjValue][k] = value;
 
                         } // Pasar al sistema americano de notación decimal          
                     };
-                    j = j + k
-                } while (j < data.length - 1);
+                    nColumns = nColumns + k
+                } while (nColumns < data.length - 1);
                 Files.insert(result)
             }
-            console.log("Stored ", data.length, " rows of data. Awesome!")
+            console.log("Stored ", data.length, " rows of data.\nAwesome!")
         };
 
         function parseData(url, callBack) {
---
+++ client/dashboard.js
@@ -0,0 +1,8 @@
+Template.dashboard.helpers({
+	fakeTitle: function() {
+		return Fake.sentence(2)
+	},
+	fakeParagraph: function() {
+		return Fake.paragraph(5)
+	}
+})
