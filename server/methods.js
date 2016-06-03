8d1e565c95aca9d5ad0da25282855073f1961aba
Antonio Rodríguez <musantrobeats@gmail.com>
2016-06-03 19:13:41 +0200

    parseRow in csv-parser

--- server/methods.js
+++ server/methods.js
@@ -1,147 +1,149 @@
 Meteor.methods({
 
     'bench': function(file) {
-        var csv = require('csv-parser')
+        var csv = require('csv-parse')
         var fs = require('fs')
 
         var now = Date.now()
         var rows = 0
 
         fs.createReadStream(file.path)
-            .pipe(csv())
+            .pipe(csv({
+                relax_column_count: true
+            }))
             .on('data', function(line) {
-              console.log(line);
-                rows++
+                console.log(rows++);
+                parseRow(line, file._id)
             })
             .on('end', function() {
-                console.log('parsed ' + rows + ' rows in ' + (Date.now() - now)/1000 + ' s')
+                console.log('parsed ' + rows + ' rows in ' + (Date.now() - now) / 1000 + ' s')
             })
-    },
 
-    'parseRow': function(row, ID) {
-        // Papaparse creates a nested array. So let's un-nest it.
-        row = row[0];
+        function parseRow(row, ID) {
+            // Papaparse creates a nested array. So let's un-nest it.
+            // row = row[0];
 
-        // Check in db where to store the row
-        var saveLocation = Tests.findOne({
-                _id: ID
-            })["current"]
-            // Do it!
-        if (isLineBreak(row)) {
-            switch (saveLocation) {
-                case "meta":
-                    saveLocation = Tests.update({
-                        _id: ID
-                    }, {
-                        $set: {
-                            ["current"]: "header"
-                        }
-                    })
-                    break;
-                case "header":
-                    saveLocation = Tests.update({
-                        _id: ID
-                    }, {
-                        $set: {
-                            ["current"]: "sensor"
-                        }
-                    })
-                    break;
-            }
-
-        } else {
-            save(row, saveLocation);
-        }
-
-
-
-        // FUNCTIONS
-        function exists(parent, prop, callback) {
-            if (Tests.findOne({
-                    '_id': ID,
-                    [parent + "." + prop]: {
-                        $exists: true
-                    }
-                })) {
-                return true;
-            } else {
-                return false;
-            };
-        };
-
-        function isLineBreak(arr) {
-            if (arr.length <= 1 && arr[0] === "") {
-                return true;
-            } else {
-                return false;
-            }
-        };
-
-        function save(row, where) {
-            var obj = {};
-
-            if (where == "meta") {
-                var key = row.shift();
-                obj = {};
-                for (var i = 0; i < row.length; ++i)
-                    obj = row[i];
-                Tests.update({
+            // Check in db where to store the row
+            var saveLocation = Tests.findOne({
                     _id: ID
-                }, {
-                    $set: {
-                        [where + "." + key]: obj
-                    }
-                });
-            } else if (where == "header") {
-                for (var i = 0; i < row.length; ++i) {
-                    obj[i] = row[i];
-                    if (!exists("sensor." + i, "name")) {
-                        Tests.update({
+                })["current"]
+                // Do it!
+            if (isLineBreak(row)) {
+                switch (saveLocation) {
+                    case "meta":
+                        saveLocation = Tests.update({
                             _id: ID
                         }, {
                             $set: {
-                                ["sensor." + i + ".name"]: obj[i],
+                                ["current"]: "header"
                             }
                         })
-                    } else if (!exists("sensor." + i, "customName")) {
-                        Tests.update({
+                        break;
+                    case "header":
+                        saveLocation = Tests.update({
                             _id: ID
                         }, {
                             $set: {
-                                ["sensor." + i + ".customName"]: obj[i],
+                                ["current"]: "sensor"
                             }
                         })
-                    } else if (!exists("sensor." + i, "units")) {
-                        Tests.update({
-                            _id: ID
-                        }, {
-                            $set: {
-                                ["sensor." + i + ".units"]: obj[i],
-                            }
-                        })
-                    } else if (!exists("sensor." + i, "number")) {
-                        Tests.update({
-                            _id: ID
-                        }, {
-                            $set: {
-                                ["sensor." + i + ".number"]: obj[i],
-                            }
-                        })
-                    };
+                        break;
+                }
+
+            } else {
+                save(row, saveLocation);
+            }
+
+
+
+            // FUNCTIONS
+            function exists(parent, prop, callback) {
+                if (Tests.findOne({
+                        '_id': ID,
+                        [parent + "." + prop]: {
+                            $exists: true
+                        }
+                    })) {
+                    return true;
+                } else {
+                    return false;
                 };
-            } else if (where == "sensor") {
-                for (var i = 0; i < row.length; ++i) {
+            };
+
+            function isLineBreak(arr) {
+                if (arr.length <= 1 && arr[0] === "") {
+                    return true;
+                } else {
+                    return false;
+                }
+            };
+
+            function save(row, where) {
+                var obj = {};
+
+                if (where == "meta") {
+                    var key = row.shift();
+                    obj = {};
+                    for (var i = 0; i < row.length; ++i)
+                        obj = row[i];
                     Tests.update({
                         _id: ID
                     }, {
-                        $push: {
-                            ["sensor." + i + ".values"]: Number(row[i]),
+                        $set: {
+                            [where + "." + key]: obj
                         }
                     });
-                };
-            }
-        };
+                } else if (where == "header") {
+                    for (var i = 0; i < row.length; ++i) {
+                        obj[i] = row[i];
+                        if (!exists("sensor." + i, "name")) {
+                            Tests.update({
+                                _id: ID
+                            }, {
+                                $set: {
+                                    ["sensor." + i + ".name"]: obj[i],
+                                }
+                            })
+                        } else if (!exists("sensor." + i, "customName")) {
+                            Tests.update({
+                                _id: ID
+                            }, {
+                                $set: {
+                                    ["sensor." + i + ".customName"]: obj[i],
+                                }
+                            })
+                        } else if (!exists("sensor." + i, "units")) {
+                            Tests.update({
+                                _id: ID
+                            }, {
+                                $set: {
+                                    ["sensor." + i + ".units"]: obj[i],
+                                }
+                            })
+                        } else if (!exists("sensor." + i, "number")) {
+                            Tests.update({
+                                _id: ID
+                            }, {
+                                $set: {
+                                    ["sensor." + i + ".number"]: obj[i],
+                                }
+                            })
+                        };
+                    };
+                } else if (where == "sensor") {
+                    for (var i = 0; i < row.length; ++i) {
+                        Tests.update({
+                            _id: ID
+                        }, {
+                            $push: {
+                                ["sensor." + i + ".values"]: Number(row[i]),
+                            }
+                        });
+                    };
+                }
+            };
 
 
-    },
+        }
+    }
 })
