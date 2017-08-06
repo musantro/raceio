3831d1b9fe3b3c9b4c5959429aa8eb7e87c2d629
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2017-08-06 17:33:35 +0200

    save friendly Names in settings
    Now it is possible to save the friendly Names through settings

--- client/settings/events.js
+++ client/settings/events.js
@@ -1,3 +1,30 @@
 Template.settings.onRendered(function() {
-    $('select').material_select();
+  $('select').material_select();
+
 })
+
+var hooksObject = {
+  onSuccess: function() {
+    if ($('.toast').first()[0] == undefined) {
+      Materialize.toast($toastContent, 3000);
+    }
+  }
+};
+var $toastContent = $('<span>Settings saved</span>');
+var formIds = Labels.find().map(function(item) {
+  return "settings-" + item._id;
+});
+AutoForm.addHooks(null, hooksObject);
+
+Template["settings"].helpers({
+  labels() {
+    return Labels.find({}, {
+      sort: {
+        name: 1
+      }
+    });
+  },
+  makeUniqueID() {
+    return `settings-${this._id}`;
+  }
+});
