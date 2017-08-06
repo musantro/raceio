10b4b22fad12a3b2209a00bb8efca71114824d00
Antonio Rodríguez <musantrobeats+dev@gmail.com>
2017-08-06 20:37:15 +0200

    delete ellipse coming soon toast

--- client/test/events.js
+++ client/test/events.js
@@ -1,5 +1,5 @@
-Template.testOverview.events({
-  'click #ellipse':function() {
-    Materialize.toast("Feature not available yet, but soon!",4000)
-  }
-})
+// Template.testOverview.events({
+//   'click #ellipse':function() {
+//     Materialize.toast("Feature not available yet, but soon!",4000)
+//   }
+// })
